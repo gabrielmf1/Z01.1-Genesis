50f7189a97723cea47e3d1d2ed2348623c3cb125
Gabriel Couto <gabrielcla@al.insper.edu.br>
2020-03-05 17:26:48 -0300

    Parte Gabriel Couto

--- Projetos/B-LogicaCombinacional/src/rtl/DMux8Way.vhd
+++ Projetos/B-LogicaCombinacional/src/rtl/DMux8Way.vhd
@@ -3,8 +3,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity DMux8Way is
 	port ( 
-			a:   in  STD_LOGIC;
-			sel: in  STD_LOGIC_VECTOR(2 downto 0);
+			a:   in  STD_LOGIC; -- entrada
+			sel: in  STD_LOGIC_VECTOR(2 downto 0); -- seletor
 			q0:  out STD_LOGIC;
 			q1:  out STD_LOGIC;
 			q2:  out STD_LOGIC;
@@ -17,6 +17,13 @@ end entity;
 
 architecture arch of DMux8Way is
 begin
-
+	q0 <= a when sel = "000" else '0';
+	q1 <= a when sel = "001" else '0';
+	q2 <= a when sel = "010" else '0';
+	q3 <= a when sel = "011" else '0';
+	q4 <= a when sel = "100" else '0';
+	q5 <= a when sel = "101" else '0';
+	q6 <= a when sel = "110" else '0';
+	q7 <= a when sel = "111" else '0';
 
 end architecture;
