363f0c95b7ea81647eee7308c26322373805668e
hthome1 <henrique.thome@hotmail.com>
2020-05-13 20:23:57 -0300

    corrigindo PC

--- Projetos/D-LogicaSequencial/src/rtl/PC.vhd
+++ Projetos/D-LogicaSequencial/src/rtl/PC.vhd
@@ -20,31 +20,49 @@ entity PC is
         load      : in  STD_LOGIC;
         reset     : in  STD_LOGIC;
         input     : in  STD_LOGIC_VECTOR(15 downto 0);
-        output    : out STD_LOGIC_VECTOR(15 downto 0)
+        output    : out STD_LOGIC_VECTOR(15 downto 0):="0000000000000000"
     );
 end entity;
 
 architecture arch of PC is
+  -- Aqui declaramos sinais (fios auxiliares)
+  -- e componentes (outros módulos) que serao
+  -- utilizados nesse modulo.
 
- signal muxOut : std_logic_vector(15 downto 0);
-
-  component Inc16 is
+ component Inc16 is
       port(
-          a   :  in STD_LOGIC_VECTOR(15 downto 0);
-          q   : out STD_LOGIC_VECTOR(15 downto 0)
-          );
+    a   :  in STD_LOGIC_VECTOR(15 downto 0);
+    q   : out STD_LOGIC_VECTOR(15 downto 0)
+  );
+  end component;
+
+  component Mux16 is
+    port ( 
+     a:   in  STD_LOGIC_VECTOR(15 downto 0);
+     b:   in  STD_LOGIC_VECTOR(15 downto 0);
+     sel: in  STD_LOGIC;
+    q:   out STD_LOGIC_VECTOR(15 downto 0)
+    );
   end component;
 
   component Register16 is
-      port(
-          clock:   in STD_LOGIC;
-          input:   in STD_LOGIC_VECTOR(15 downto 0);
-          load:    in STD_LOGIC;
-          output: out STD_LOGIC_VECTOR(15 downto 0)
-        );
-    end component;
+  port(
+    clock:   in STD_LOGIC;
+    input:   in STD_LOGIC_VECTOR(15 downto 0);
+    load:    in STD_LOGIC;
+    output: out STD_LOGIC_VECTOR(15 downto 0)
+  );
+  end component;
+
+  
+  SIGNAL  preout, incout, muxincout, muxloadout, muxresetout, registerout: std_logic_vector(15 downto 0):="0000000000000000";
 
 begin
-
-
+  inc: Inc16 port map (preout, incout);
+  muxinc: Mux16 port map(preout, incout, increment, muxincout);
+  muxload: Mux16 port map(muxincout, input, load, muxloadout);
+  muxreset: Mux16 port map(muxloadout, "0000000000000000", reset, muxresetout);
+  registerf: Register16 port map (clock, muxresetout, '1', preout);
+  output <= preout;
+    
 end architecture;
